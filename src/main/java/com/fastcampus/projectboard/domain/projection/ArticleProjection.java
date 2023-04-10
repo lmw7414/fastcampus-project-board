5e8a1a0edb74bbf90bec59c16be16eb96886c97a
Lee Min Woo <lmw7414@naver.com>
2023-04-10 17:07:23 +0900

    #97 - 프로젝션 필드의 타입 변경
    내가 왜 그랬는지는 모르겠으나 UserAccount의 필드 타입을 잘못 입력하였다. 이에 따른 오류 수정

--- src/main/java/com/fastcampus/projectboard/domain/projection/ArticleProjection.java
+++ src/main/java/com/fastcampus/projectboard/domain/projection/ArticleProjection.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.config.Projection;
 
 import java.time.LocalDateTime;
 
-@Projection(name = "withUserAccount", types = Article.class)
+@Projection(name ="withUserAccount", types = Article.class)
 public interface ArticleProjection {
     Long getId();
     UserAccount getUserAccount();
